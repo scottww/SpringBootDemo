7715c38a76c201b25a9c40be2ffad1bd084f006f
scottww <798868678@qq.com>
2019-07-13 01:56:59 +0800

    [MOD]application.properties配置及获取application.properties配置的值

--- src/main/java/com/example/demo/web/HelloWorldHandler.java
+++ src/main/java/com/example/demo/web/HelloWorldHandler.java
@@ -1,5 +1,6 @@
 package com.example.demo.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,14 @@ public class HelloWorldHandler {
     @RequestMapping(value = "/index")
     public String index(){
         return "Hello World !";
+    }
+
+    //获取application.properties配置的值
+    @Value("${shyy.msg}")
+    private String msg;
+
+    @RequestMapping(value = "/showMsg")
+    public String showMsg() {
+        return this.msg;
     }
 }
